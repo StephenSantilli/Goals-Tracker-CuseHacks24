3d4d500512f1f2eef2eac50c2564e9e33ccfeffa
Stephen Santilli <stephen.j.santilli@gmail.com>
2024-10-05 17:23:22 -0400

    now it works

--- hackathon.js
+++ hackathon.js
@@ -1,4 +1,4 @@
-function storeInput(difficulty) {
+function storeInput() {
 
 
     // Get the input elements
@@ -6,12 +6,14 @@ function storeInput(difficulty) {
     const goalInput = document.getElementById("goal");
     const taskInput = document.getElementById("task");
     const dateInput = document.getElementById("Due");
+    const difficultyInput = document.getElementById("difficulty");
     console.log(dateInput)
     // Get the values of the input elements
     let name = nameInput.value;
     let goal = goalInput.value;
     let task = taskInput.value;
     let date = dateInput.value;
+    let difficulty = difficultyInput.value;
 
     $.ajax({
         type: "POST",
