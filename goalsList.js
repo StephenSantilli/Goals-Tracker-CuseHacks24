0347c43872d4d06f870ae4b388e028a1348656e1
Stephen Santilli <stephen.j.santilli@gmail.com>
2024-10-05 18:20:54 -0400

    goal now titles

--- goalsList.js
+++ goalsList.js
@@ -17,19 +17,28 @@ request.done((res) => {
     for (let i = 0; i < data.length; i++) {
         let g = data[i];
 
-        let row = list.insertRow();
+        let title = document.createElement("h2")
+        title.innerHTML = g.goal;
+        document.body.appendChild(title)
 
-        let goalCell = row.insertCell(0);
-        goalCell.innerHTML = g.goal
+        let list = document.createElement("table")
+
+        let titleRow = list.insertRow();
+        titleRow.insertCell(0).innerHTML = "Task"
+        titleRow.insertCell(1).innerHTML = "Due"
+        titleRow.insertCell(2).innerHTML = "Difficulty"
+        titleRow.insertCell(3).innerHTML = "Done"
+
+        document.body.appendChild(list);
+
         for (let j = 0; j < g.tasks.length; j++) {
 
             let t = g.tasks[j]
             let row = list.insertRow();
-            row.insertCell(0)
-            let taskCell = row.insertCell(1);
-            let dueCell = row.insertCell(2);
-            let difficultyCell = row.insertCell(3);
-            let doneCell = row.insertCell(4);
+            let taskCell = row.insertCell(0);
+            let dueCell = row.insertCell(1);
+            let difficultyCell = row.insertCell(2);
+            let doneCell = row.insertCell(3);
             taskCell.innerHTML = t.task
             dueCell.innerHTML = t.date
             difficultyCell.innerHTML = t.difficulty
