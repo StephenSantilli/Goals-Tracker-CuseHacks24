07d5a08b868eb662f629e196bb8ee819484b7f26
Kimberly Talisse <ketaliss@syr.edu>
2024-10-05 15:51:45 -0400

    point calculations

--- GoalPoints.js
+++ GoalPoints.js
@@ -1,1 +1,11 @@
 const buttons = document.getElementByClassName("cbut");
+
+// looping through all ten buttons
+for (let i = 0; i < buttons.length; i++ ){
+    buttons[i].addEventListener("click", function(){
+        const buttonValue = buttons[i].value;
+    });
+}
+
+// assigning point values for button pushed
+const points = buttonValue * 10;
